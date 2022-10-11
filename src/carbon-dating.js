a38afd387bcddc0feb0882103d13ffc153494634
Buldar <buldar666@gmail.com>
2022-10-11 17:40:12 +0300

    carbon-dating.js finished

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample( sampleActivity) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!sampleActivity) {return false}
+  if (typeof sampleActivity!=='string') {return false}
+  if (!Number(sampleActivity)) {return false}
+  if (sampleActivity<=0 || sampleActivity>=15) {return false}
+  return Math.ceil((Math.log(15/sampleActivity))/(0.693/5730))
 }
 
 module.exports = {
