d1551987dd9517318246073c17d249487a1b0b60
Buldar <buldar666@gmail.com>
2022-10-12 16:39:10 +0300

    dns-stats.js done

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+function getDNSStats( domains ) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let result = {}
+  for (let i = 0; i<domains.length; i++) {
+    let newArr = domains[i].split('.').map(x=>'.'+x)
+    let currId = newArr.length-1
+    let currDomain=''
+    while (currId>=0) {
+      currDomain = currDomain + newArr[currId];
+      (!result[currDomain]?result[currDomain]=1:result[currDomain]++)
+      currId--
+    }
+  }
+  return result
 }
 
 module.exports = {
